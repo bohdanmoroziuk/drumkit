3ec9cdf759dae7d9d4d77796a13d9c6c46da06ba
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-04-05 14:12:44 +0300

    feat: add ability to start/stop auto music

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 let audioVolume = 0.6;
 
+let autoMusicIntervalId = null;
+let autoMusicOn = false;
+
 const drums = document.querySelectorAll('.drum');
 
 const volumeSlider = document.querySelector('#volume');
+
+const startAutoMusicButton = document.querySelector('#start-auto-music');
+
+const keys = Array.from(drums).map((drum) => drum.innerHTML);
 
 const handleVolumeSliderInput = (event) => {
   audioVolume = event.target.value / 100;
@@ -39,8 +46,34 @@ const handleDrumClick = (event) => {
   makeSound(key);
 };
 
+const startAutoMusic = () => {
+  autoMusicIntervalId = setInterval(() => {
+    const key = keys[Math.floor(Math.random() * keys.length)];
+
+    animate(key);
+    makeSound(key);
+  }, 300);
+};
+
+const handleStartAutoMusicButtonClick = () => {
+  if (autoMusicOn) {
+    clearInterval(autoMusicIntervalId);
+    startAutoMusicButton.innerHTML = 'Start Auto Music';
+    startAutoMusicButton.classList.remove('pressed');
+    autoMusicOn = false;
+    return;
+  }
+
+  startAutoMusicButton.innerHTML = 'Stop Auto Music';
+  startAutoMusicButton.classList.add('pressed');
+  autoMusicOn = true;
+  startAutoMusic();
+}; 
+
 drums.forEach((drum) => {
   drum.addEventListener('click', handleDrumClick);
 });
 
 volumeSlider.addEventListener('input', handleVolumeSliderInput);
+
+startAutoMusicButton.addEventListener('click', handleStartAutoMusicButtonClick);
